8b728207fa130dbae48f63a1f23bf680aa23af42
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-04 10:25:52 -0300

    Create __main__.py

---
+++ plugins/weather/clima/__main__.py
@@ -0,0 +1,24 @@
+from pyrogram.errors import YouBlockedUser
+
+from userge import Message, userge
+
+@userge.on_cmd(
+    "clima",
+    about={
+        "header": "Weather of the city",
+        "description": "Busque o clima para uma determinada cidade.",
+        "usage": "{tr}clima [text]",
+    },
+)
+async def weather(message: Message):
+    city_ = m.input_str
+    bot_ = "@WhiterKangBOT"
+   async with userge.conversation(bot_, timeout=1000) as conv:
+       try:
+           await conv.send_message(f"!clima {city_}")
+       except YouBlockedUser:
+           await message.reply("Por Favor desbloqueie a @WhiterKangBOT")
+           return
+        response = await conv.get_response(mark_read=False)
+        await message.edit(response.text)
+               
