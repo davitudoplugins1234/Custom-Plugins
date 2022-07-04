29cbdd6e6b18267524b269cc3cbb60f4f051c988
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2022-07-04 10:44:04 -0300

    Update __main__.py

--- plugins/weather/clima/__main__.py
+++ plugins/weather/clima/__main__.py
@@ -13,12 +13,12 @@ from userge import Message, userge
 async def weather(message: Message):
     city_ = m.input_str
     bot_ = "@WhiterKangBOT"
-   async with userge.conversation(bot_, timeout=1000) as conv:
-       try:
-           await conv.send_message(f"!clima {city_}")
-       except YouBlockedUser:
-           await message.reply("Por Favor desbloqueie a @WhiterKangBOT")
-           return
+    async with userge.conversation(bot_, timeout=1000) as conv:
+        try:
+            await conv.send_message(f"!clima {city_}")
+        except YouBlockedUser:
+            await message.reply("Por Favor desbloqueie a @WhiterKangBOT")
+            return
         response = await conv.get_response(mark_read=False)
         await message.edit(response.text)
                
